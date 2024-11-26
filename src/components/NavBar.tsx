f7b3291a7b025bea886a0dea6141c0f5f1472766
KOREDE BUSUYI <89644512+Korexszman@users.noreply.github.com>
2024-11-26 09:06:37 +0100

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -94,7 +94,7 @@ const NavBar = () => {
             </button>
 
             <a
-              href="https://salesforce.obana.africa"
+              href="https://salesforce.obana.africa/"
               target="_blank"
               rel="noopener noreferrer"
               className="text-obGray-700 hover:text-primary flex items-center min-h-[3.1875rem]"
@@ -102,7 +102,7 @@ const NavBar = () => {
               Join Our Sales Team
             </a>
             <a
-              href="#"
+              href="https://vendor.obana.africa/"
               target="_blank"
               rel="noopener noreferrer"
               className="text-obGray-700 hover:text-primary flex items-center min-h-[3.1875rem]"
