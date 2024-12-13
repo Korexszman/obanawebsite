df46369c974e800621494e1cf27db0a50a177ecf
KOREDE BUSUYI <89644512+Korexszman@users.noreply.github.com>
2024-12-13 09:42:22 +0100

    Update HeroBanner.tsx

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -14,7 +14,7 @@ const HeroBanner = () => {
 						</h1>
 
 						<p className="text-lg sm:text-xl mb-8 text max-w-3xl mx-auto">
-						"We enable Nigerian SMEs in the fashion, beauty and lifestyle industries to scale their businesses by being all-in-one sourcing platform"
+						"We enable Nigerian SMEs in the fashion, beauty and lifestyle industries to scale their businesses by providing an all-in-one sourcing platform"
 						</p>
 						<div className="flex justify-center space-x-1 max-sm:flex-col items-center">
 							<Button className="w-[200px]" icon onClick={() => window.open("https://shop.obana.africa/", "_blank")}>
